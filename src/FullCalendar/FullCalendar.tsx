56c66b607f26ff30fa4b49965c0f113ad9ed143f
FAIZANN AHMAD <faizann@simplusinnovation.com>
2021-03-03 18:37:55 +0500

    recurring events generated

--- src/FullCalendar/FullCalendar.tsx
+++ src/FullCalendar/FullCalendar.tsx
@@ -4,6 +4,8 @@ import dayGridPlugin from '@fullcalendar/daygrid'
 import timeGridPlugin from '@fullcalendar/timegrid'
 import listPlugin from '@fullcalendar/list';
 import interactionPlugin from "@fullcalendar/interaction";
+import rrulePlugin from '@fullcalendar/rrule'
+
 
 
 
@@ -17,22 +19,53 @@ export function FullCalendarComponent () {
     const handleChange = (e: any) => {
         console.log(e)
     }
+
+    const handleEventClick = (e: any) => {
+        alert(e.event.title)
+    }
     return(
         <div>
             <FullCalendar
-                plugins={[ dayGridPlugin, timeGridPlugin, interactionPlugin, listPlugin ]}
+                plugins={[ rrulePlugin, dayGridPlugin, timeGridPlugin, interactionPlugin, listPlugin ]}
                 headerToolbar={{
                     left: 'prev,next,today',
                     center: 'title',
                     right: 'dayGridMonth,dayGridWeek,timeGridDay,listYear'
                 }}
+                buttonText={{list: 'year'}}
                 initialView="dayGridMonth"
+                editable={true}
                 dateClick={handleDateClick}
+                eventClick={handleEventClick}
                 eventChange={handleChange}
                 events={[
-                    { title: 'event 1', editable: true, start: '2021-03-01T05:30:00Z', end: '2021-03-01T10:30:00Z' },
-                    { title: 'event 2', date: '2021-03-02' },
-                    { title: 'event 3', date: '2020-03-02' }
+                    { title: 'event 1', start: '2021-03-01T05:30:00Z', end: '2021-03-01T10:30:00Z', manager: 'Faizan', backgroundColor: 'red' },
+                    { title: 'event 2', date: '2021-03-02', backgroundColor: 'green' },
+                    { title: 'event 3', duration: '02:00',
+                        rrule: {
+                            freq: 'monthly',
+                            interval: 1,
+                            dtstart: '2021-01-10T05:30:00', // will also accept '20120201T103000'
+                            until: '2021-05-01' // will also accept '20120201'
+                        }
+                    },
+                    { title: 'event 4', date: '2021-03-03' },
+                    { title: 'event 5',
+                        rrule: {
+                            freq: 'monthly',
+                            interval: 1,
+                            dtstart: '2021-01-11',
+                            until: '2021-05-12'
+                        }
+                    },
+                    { title: 'event 6', groupId: '6',
+                        rrule: {
+                            freq: 'weekly',
+                            interval: 1,
+                            dtstart: '2021-01-11',
+                            until: '2021-05-12'
+                        }
+                    }
                 ]}
             />
         </div>
