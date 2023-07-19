2b425efaf4cf9dc08711cba8a53c424d3dfaa283
Faizann Ahmad <faizann@simplusinnovation.com>
2023-07-19 13:00:35 +0500

    timeline views added

--- src/FullCalendar/FullCalendar.tsx
+++ src/FullCalendar/FullCalendar.tsx
@@ -5,6 +5,7 @@ import timeGridPlugin from '@fullcalendar/timegrid'
 import listPlugin from '@fullcalendar/list';
 import interactionPlugin from "@fullcalendar/interaction";
 import rrulePlugin from '@fullcalendar/rrule'
+import resourceTimelinePlugin from '@fullcalendar/resource-timeline'
 
 
 
@@ -25,24 +26,24 @@ export function FullCalendarComponent () {
     }
 
     const events = [
-        { title: 'event 1', start: '2021-03-01T05:30:00Z', end: '2021-03-01T10:30:00Z', manager: 'Faizan', backgroundColor: 'red' },
-        { title: 'event 2', date: '2021-03-02', backgroundColor: 'green' },
+        { title: 'event 1', resourceId: 'a', start: '2023-07-01T05:30:00Z', end: '2023-07-01T10:30:00Z', manager: 'Faizan', backgroundColor: 'red' },
+        { title: 'event 2',  resourceId: 'b', date: '2023-07-02', backgroundColor: 'green' },
         // recurring event
         { title: 'event 3', duration: '02:00',
             rrule: {
                 freq: 'monthly',
                 interval: 1,
-                dtstart: '2021-01-10T05:30:00', // will also accept '20120201T103000'
-                until: '2021-05-01' // will also accept '20120201'
+                dtstart: '2023-01-10T05:30:00', // will also accept '20120201T103000'
+                until: '2023-05-01' // will also accept '20120201'
             }
         },
-        { title: 'event 4', date: '2021-03-03' },
+        { title: 'event 4', date: '2023-07-03' },
         { title: 'event 5',
             rrule: {
                 freq: 'monthly',
                 interval: 1,
-                dtstart: '2021-01-11',
-                until: '2021-05-12'
+                dtstart: '2023-01-11',
+                until: '2023-05-12'
             }
         },
         // recurring event which can move together
@@ -50,20 +51,20 @@ export function FullCalendarComponent () {
             rrule: {
                 freq: 'weekly',
                 interval: 1,
-                dtstart: '2021-01-11',
-                until: '2021-05-12'
+                dtstart: '2023-01-11',
+                until: '2023-05-12'
             }
         },
         // red areas where you cannot move event
         {
-            start: '2021-03-24',
-            end: '2021-03-28',
+            start: '2023-07-24',
+            end: '2023-07-28',
             overlap: false,
             display: 'background',
             color: '#ff9f89'
         },
         {
-            date: '2021-03-06',
+            date: '2023-07-06',
             editable: true,
             title: 'weekend',
             overlap: false,
@@ -74,14 +75,26 @@ export function FullCalendarComponent () {
     return(
         <div>
             <FullCalendar
-                plugins={[ rrulePlugin, dayGridPlugin, timeGridPlugin, interactionPlugin, listPlugin ]}
+                plugins={[ rrulePlugin, dayGridPlugin, timeGridPlugin, interactionPlugin, listPlugin, resourceTimelinePlugin ]}
+                schedulerLicenseKey="CC-Attribution-NonCommercial-NoDerivatives"
                 headerToolbar={{
                     left: 'prev,next,today',
                     center: 'title',
-                    right: 'dayGridMonth,dayGridWeek,timeGridDay,listYear'
+                    right: 'dayGridMonth,dayGridWeek,timeGridDay,listYear, resourceTimelineWeek,resourceTimelineMonth'
                 }}
                 buttonText={{list: 'year'}}
-                initialView="dayGridMonth"
+                resourceAreaColumns={[
+                    {
+                      field: 'title',
+                      headerContent: 'Room'
+                    }
+                  ]}
+                resources={[
+                    { id: 'a', title: 'Resource A' },
+                    { id: 'b', title: 'Resource B' },
+                    // Add more resource objects as needed
+                  ]}
+                initialView="resourceTimelineWeek"
                 editable={true}
                 dateClick={handleDateClick}
                 eventClick={handleEventClick}
